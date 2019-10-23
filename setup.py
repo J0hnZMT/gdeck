a5873a73f17c2c13b8fe679050f752085ae7e206
J0hnZMT <tuddaojohnzel@gmail.com>
2019-10-23 10:24:54 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='gdeck',
+      version='0.1',
+      description='Store parent paths, File names, size and hashes of the selected dir to a csv file.',
+      url='https://github.com/J0hnZMT/gdeck',
+      author='Johnzel Tuddao',
+      author_email='tuddaojohnzel@gmail.com',
+      license='MIT',
+      classifiers=[
+                  'Development Status :: 3 - Alpha',
+                  'License :: MIT License',
+                  'Intended Audience :: Education',
+                  'Topic :: Security :: Cryptography',
+                  'Programming Language :: Python 3',
+                  'Programming Language :: Python 3.7'],
+      keywords='Deck of cards',
+      packages=['gdeck'],
+      zip_safe=False,
+      )
